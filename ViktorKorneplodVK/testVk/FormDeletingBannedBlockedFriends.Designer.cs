026c5c68f787c7ca941ea0dbdfaca03b9815ad14
Maxim7686 <104256823+Maxim7686@users.noreply.github.com>
2022-05-01 17:09:19 +0300

    Add files via upload

--- ViktorKorneplodVK/testVk/FormDeletingBannedBlockedFriends.Designer.cs
+++ ViktorKorneplodVK/testVk/FormDeletingBannedBlockedFriends.Designer.cs
@@ -28,27 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(260, 191);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(124, 191);
+            this.button1.Location = new System.Drawing.Point(100, 191);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(128, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "показать друзей";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -60,14 +52,35 @@
             this.textBox1.Size = new System.Drawing.Size(389, 370);
             this.textBox1.TabIndex = 3;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "удалить banned";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(100, 220);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "показать забаненных";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // FormDeletingBannedBlockedFriends
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Name = "FormDeletingBannedBlockedFriends";
             this.Text = "FormDeletingBannedBlockedFriends";
             this.Load += new System.EventHandler(this.FormDeletingBannedBlockedFriends_Load);
@@ -77,9 +90,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- ViktorKorneplodVK/testVk/FormDeletingBannedBlockedFriends.cs
+++ ViktorKorneplodVK/testVk/FormDeletingBannedBlockedFriends.cs
@@ -24,7 +24,7 @@ namespace testVk
 
         private void FormDeletingBannedBlockedFriends_Load(object sender, EventArgs e)
         {
-            label1.Text = access_token;
+         
         }
 
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
@@ -33,6 +33,9 @@ namespace testVk
 
         private void button1_Click(object sender, EventArgs e)
         {
+        // мои запросы https://api.vk.com/method/friends.get?fields=bdate&access_token=7d904d438e43e09168ababf069fba93c9c78c7483621c770a41a2035f9721d95ae14e2a9e286186ab5957&v=5.131
+        https://api.vk.com/method/friends.delete&access_token=bb98347f29c98cb85c27b21865a0bf2a8e6c540db6b665373956aea2d0b2b5b118bf9ab67b5189b30aa4a&v=5.131
+            textBox1.Text = "";
             WebClient client = new WebClient();
             string answer = Encoding.UTF8.GetString(client.DownloadData(
                    "https://api.vk.com/method/friends.get?"
@@ -40,9 +43,73 @@ namespace testVk
                    + access_token
                    + "&v=5.131"
                    ));
-            textBox1.Text = answer;
-            friendList List = JsonConvert.DeserializeObject<friendList>(answer);
-            textBox1.Text = List.response.bdate;
+            //textBox1.Text = answer;
+            friendList friends = JsonConvert.DeserializeObject<friendList>(answer);
+            //textBox1.Text = List.response.bdate;
+
+            foreach (friendList.Item friend in friends.response.items)
+            {
+                if (friend.deactivated != null)
+                {
+                    textBox1.Text = textBox1.Text + friend.first_name + " " + friend.id.ToString() + "\r\n";
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            string answer = Encoding.UTF8.GetString(client.DownloadData(
+                   "https://api.vk.com/method/friends.get?"
+                   + "fields=bdate&"
+                   + access_token
+                   + "&v=5.131"
+                   ));
+            string delete = Encoding.UTF8.GetString(client.DownloadData(
+                   "https://api.vk.com/method/friends.delete?"
+                   + "user_id=44273152&"
+                   + access_token
+                   + "&v=5.131"
+                   ));
+            friendList friends = JsonConvert.DeserializeObject<friendList>(answer);
+            int couner = 0;
+            foreach (friendList.Item friend in friends.response.items)
+            {
+              textBox1.Text = textBox1.Text + friend.first_name + " " + friend.deactivated + "" + friend.id.ToString() + "\r\n";
+              couner++;
+              delete = Encoding.UTF8.GetString(client.DownloadData(
+              "https://api.vk.com/method/friends.delete?"
+              + "user_id&"
+              + access_token
+              + "&v=5.131"
+              ));
+                if (couner > 2)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            WebClient client = new WebClient();
+            string answer = Encoding.UTF8.GetString(client.DownloadData(
+                   "https://api.vk.com/method/friends.get?"
+                   + "fields=bdate&"
+                   + access_token
+                   + "&v=5.131"
+                   ));
+            //textBox1.Text = answer;
+            friendList friends = JsonConvert.DeserializeObject<friendList>(answer);
+            //textBox1.Text = List.response.bdate;
+            foreach (friendList.Item friend in friends.response.items)
+            {
+                if (friend.deactivated != null)
+                {
+                    textBox1.Text = textBox1.Text + friend.first_name + " " + friend.deactivated + "" + friend.id.ToString() + "\r\n";
+                }
+            }
         }
     }
 }
